8be9f43c99db91961a33cfea4e3b310f46acfe1c
codenamezeta <michael@a2zeta.com>
2024-09-16 10:58:03 -0700

    Adds google analytics tag to _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,11 +1,22 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 
 class MyDocument extends Document {
   render() {
     return (
       <Html lang='en'>
         <Head>
-          <script async src='https://js.stripe.com/v3/buy-button.js'></script>
+          <Script
+            async
+            src='https://www.googletagmanager.com/gtag/js?id=G-PZ2V2Q2WL2'
+          ></Script>
+          <Script>
+            {`
+            window.dataLayer = window.dataLayer || []; function gtag()
+            {dataLayer.push(arguments);}
+            gtag('js', new Date()); gtag('config', 'G-PZ2V2Q2WL2');`}
+          </Script>
+          {/* <script async src='https://js.stripe.com/v3/buy-button.js'></script> */}
           {/* <link
             rel='icon'
             type='image/png'
